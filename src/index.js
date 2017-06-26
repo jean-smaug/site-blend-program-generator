e26c6bf1efce1e00dc9af8e825a537a64a0c6eb2
the-smaug <maximeblanc.dev@gmail.com>
2017-06-26 10:56:43 +0200

    Create react app (#12)
    * init
    
    * add lodash
    
    * add lockfile
    
    * remove yarn lint in travis for the moment
    
    * resolve git conflict

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 
-import { AppContainer } from 'react-hot-loader';
-// AppContainer is a necessary wrapper component for HMR
-
-import App from './components/App';
-
-/* eslint-disable */
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('app'),
-  );
-};
-/* eslint-enable */
-
-render(App);
-
-// Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    render(App);
-  });
-}
+ReactDOM.render(<App />, document.getElementById("root"));
+registerServiceWorker();
