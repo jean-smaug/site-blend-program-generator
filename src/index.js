f5d1cb9c7975515d2da6baa6d957d7c710f0a739
maxime blanc <maximeblanc.dev@gmail.com>
2017-06-26 10:42:41 +0200

    resolve git conflict

--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-<<<<<<< HEAD
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 
-import { AppContainer } from 'react-hot-loader';
-// AppContainer is a necessary wrapper component for HMR
-
-import App from './components/App';
-
-/* eslint-disable */
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    document.getElementById('app'),
-  );
-};
-/* eslint-enable */
-
-render(App);
-
-// Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./components/App', () => {
-    render(App);
-  });
-}
-=======
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
->>>>>>> 16e79f8... init
