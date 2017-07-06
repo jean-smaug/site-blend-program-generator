5ae986013ea2fa7a49509f7abbf72361a3fe46f7
Maxime Chabert <sin2.chabert.maxime@gmail.com>
2017-07-06 14:18:02 +0200

    Initialisation de redux (#15)
    * Initialisation de redux. Ajout dépendances + création premier reducer/actions/stores etc..
    
    * Fix syntaxte + english + fix text for travis
    
    * forget a word
    
    * remove test
    
    * forget a word

---
+++ src/components/App/App.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  componentDidMount () {
+    console.log(this.props.state.form);
+    this.props.addKeyword("testaha");
+
+  }
+
+  render() {
+    console.log(this.props.state.form);
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to blend</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
