3c8f5d808f4032d9dd4e71bd812af05010e37c50
maxime blanc <maximeblanc.dev@gmail.com>
2017-07-17 22:09:43 +0200

    add comments

--- src/lib/filter.js
+++ src/lib/filter.js
@@ -1,7 +1,21 @@
 import _ from "lodash";
 
+/**
+ *
+ * @param {Object} data
+ * @param {String} domain
+ */
 export const filterByDomain = (data, domain) =>
   _.filter(data, item => item.domain === domain);
 
+/**
+ *
+ * @param {Object} data
+ * @param {String} level
+ */
 export const filterByLevel = (data, level) =>
   _.filter(data, item => item.level === level);
+
+export const filterByLevelAndDomain = (data, filters) => {
+  return _.filter(data);
+};
