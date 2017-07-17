243cde303b8e5d20c54e8beb58cda7bc53da8dc5
the-smaug <maximeblanc.dev@gmail.com>
2017-07-17 23:24:40 +0200

    Add data (#17)
    * add data
    
    * init lib (#18)
    
    * init lib
    
    * add comments
    
    * add filterByLevelAndDomain
    
    * filter by level and domain OK

---
+++ src/lib/filter.js
@@ -0,0 +1,34 @@
+import _ from "lodash";
+
+/**
+ * Return conferences filtered by domain
+ * @param {Object} data
+ * @param {String} domain
+ */
+export const filterByDomain = (data, domain) =>
+  _.filter(data, item => item.domain === domain);
+
+/**
+ * Return conferences filtered by level
+ * @param {Object} data
+ * @param {String} level
+ */
+export const filterByLevel = (data, level) =>
+  _.filter(data, item => item.level === level);
+
+/**
+ *
+ * @param {Object} data
+ * @param {Array} filters
+ */
+export const filterByLevelAndDomain = (data, filters = []) =>
+  _.filter(data, item => {
+    return _.includes(
+      _.map(filters, filter => {
+        if (filter.domain === item.domain && filter.level === item.level) {
+          return true;
+        }
+      }),
+      true
+    );
+  });
