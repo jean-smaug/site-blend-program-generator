4126922343528bfa25bcc5de3afcd038535fde4e
maxime blanc <maximeblanc.dev@gmail.com>
2017-07-22 14:48:59 +0200

    fix lint

--- src/components/Menu/__tests__/Menu.test.js
+++ src/components/Menu/__tests__/Menu.test.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Menu from "../Menu.jsx";
+import React from 'react';
+import { shallow } from 'enzyme';
+import Menu from '../Menu';
 
-describe("Menu component", () => {
-  it("should render Menu Component", () => {
+describe('Menu component', () => {
+  it('should render Menu Component', () => {
     const MenuComponent = shallow(<Menu />);
     expect(MenuComponent).toMatchSnapshot();
   });
