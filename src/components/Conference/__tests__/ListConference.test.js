0de353bbc42176aca954775cf79b2a144a0b8097
maxime blanc <maximeblanc.dev@gmail.com>
2017-07-22 17:07:56 +0200

    add list conference component

--- src/components/Conference/__tests__/ListConference.test.js
+++ src/components/Conference/__tests__/ListConference.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Conference from '../Conference';
+import ListConference from '../ListConference';
+
+const conferences = [];
 
 describe('conference component', () => {
-  it('should contain a switchButton and lockButton', () => {
-    const conference = shallow(<Conference title="foo" />);
+  it('should ', () => {
+    const conference = shallow(<ListConference conferences={conferences} />);
     expect(conference.find('.switchButton').length).toBe(1);
     expect(conference.find('.lockButton').length).toBe(1);
   });
