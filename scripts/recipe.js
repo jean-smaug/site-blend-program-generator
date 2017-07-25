a529f907fc4c54d7a9b17d2aa7ce3075b92e2abc
maxime blanc <maximeblanc.dev@gmail.com>
2017-07-25 20:38:17 +0200

    add git branch

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -5,7 +5,14 @@ const travis = new Travis({
   version: '2.0.0',
 });
 
+const ghPagesConfig = {
+  repo: 'https://github.com/the-smaug/site-blend-program-generator',
+};
+
 travis.repos('the-smaug', 'site-blend-program-generator').get((err, res) => {
   const lastBuildNumber = res.repo.last_build_number;
+
+  ghPages.publish('build');
+
   console.log(res);
 });
