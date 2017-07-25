ed4c4649cfef7010f3165796f051881ea3f70a1a
maxime blanc <maximeblanc.dev@gmail.com>
2017-07-25 20:26:21 +0200

    add timebegin and time end to conference component

--- src/components/Conference/Conference.jsx
+++ src/components/Conference/Conference.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropType from 'prop-types';
 
-const Conference = ({ name }) => (
+const Conference = ({ name, timeBegin, timeEnd }) => (
   <div>
     <button className="switchButton">Switch</button>
-    <h4>{name}</h4>
+    <span>{`${timeBegin} - ${timeEnd}`}</span>
     <button className="lockButton">Lock</button>
+    <span>{name}</span>
   </div>
 );
 
 Conference.propTypes = {
   name: PropType.string.isRequired,
+  timeBegin: PropType.string.isRequired,
+  timeEnd: PropType.string.isRequired,
 };
 
 export default Conference;
