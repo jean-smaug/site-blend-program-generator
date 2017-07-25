7c6623414d95848ba6787228f6ace79544a47851
maxime blanc <maximeblanc.dev@gmail.com>
2017-07-25 20:47:17 +0200

    update script

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,5 +1,6 @@
 const Travis = require('travis-ci');
 const ghPages = require('gh-pages');
+const gitBranch = require('git-branch');
 
 const travis = new Travis({
   version: '2.0.0',
@@ -7,12 +8,16 @@ const travis = new Travis({
 
 const ghPagesConfig = {
   repo: 'https://github.com/the-smaug/site-blend-program-generator',
+  branch: gitBranch.sync(),
+  dest: 'recipe',
 };
 
 travis.repos('the-smaug', 'site-blend-program-generator').get((err, res) => {
   const lastBuildNumber = res.repo.last_build_number;
 
-  ghPages.publish('build');
+  ghPages.publish('build', ghPagesConfig, (errorPublish) => {
+    errorPublish !== null ? console.error(err) : console.log(err);
+  });
 
   console.log(res);
 });
