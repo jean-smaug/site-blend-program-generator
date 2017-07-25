0f45c7b68218a9214ffdacd704570d6913a478d2
maxime blanc <maximeblanc.dev@gmail.com>
2017-07-25 21:24:17 +0200

    basic smoothie component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import FormContainer from '../Form/FormContainer';
-import MenuContainer from '../Menu/MenuContainer';
+import SmoothieContainer from '../Smoothie/SmoothieContainer';
 
 const App = () => (
   <div className="App">
@@ -9,7 +9,7 @@ const App = () => (
       <h2>Personnaliser votre Blend</h2>
     </div>
     <FormContainer />
-    <MenuContainer />
+    <SmoothieContainer />
   </div>
 );
 
--- src/components/Smoothie/SmoothieContainer.jsx
+++ src/components/Smoothie/SmoothieContainer.jsx
@@ -1,4 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import ListConference from '../Conference/ListConference';
 
-export default () => <div><Menu /></div>;
+const Smoothie = ({ dayOne, dayTwo }) => (
+  <div>
+    <ListConference {...dayOne} />
+    <ListConference {...dayTwo} />
+  </div>
+);
+
+Smoothie.propTypes = {
+  dayOne: PropTypes.array.isRequired,
+  dayTwo: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = state => ({
+  dayOne: state.dayOne,
+  dayTwo: state.dayTwo,
+});
+
+export default connect(mapStateToProps)(Smoothie);
