0385e088e8a2c2a23737dccb8021e80b48f3710c
maxime blanc <maximeblanc.dev@gmail.com>
2017-07-25 18:51:39 +0200

    get last travis build

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,5 +1,11 @@
 const Travis = require('travis-ci');
+const ghPages = require('gh-pages');
 
 const travis = new Travis({
   version: '2.0.0',
+});
+
+travis.repos('the-smaug', 'site-blend-program-generator').get((err, res) => {
+  const lastBuildNumber = res.repo.last_build_number;
+  console.log(res);
 });
