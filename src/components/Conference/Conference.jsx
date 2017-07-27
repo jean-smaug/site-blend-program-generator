084692e2cec4a0ba5b4f5bed335dd387ae740f98
Chabert Maxime <sin2.chabert.maxime@gmail.com>
2017-07-27 16:55:53 +0200

    fix lint 2

--- src/components/Conference/Conference.jsx
+++ src/components/Conference/Conference.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import PropType from 'prop-types';
 
-const Conference = (name, timeBegin, timeEnd) => (
-  <div>
-    <button className="switchButton">Switch</button>
-    <span>{`${timeBegin} - ${timeEnd}`}</span>
-    <button className="lockButton">Lock</button>
-    <span>{name}</span>
-  </div>
-    );
-
+const Conference = (props) => {
+  const { name, timeBegin, timeEnd } = props;
+  return (
+    <div>
+      <button className="switchButton">Switch</button>
+      <span>{`${timeBegin} - ${timeEnd}`}</span>
+      <button className="lockButton">Lock</button>
+      <span>{name}</span>
+    </div>
+  );
+};
 Conference.propTypes = {
   name: PropType.string.isRequired,
   timeBegin: PropType.string.isRequired,
