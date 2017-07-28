e2d41d7127afbf3ca24807456ef685f4bc7d1842
Chabert Maxime <sin2.chabert.maxime@gmail.com>
2017-07-28 11:57:52 +0200

    début des test pour génération d'un planning pdf

--- src/components/Smoothie/SmoothieContainer.jsx
+++ src/components/Smoothie/SmoothieContainer.jsx
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import JsPdf from 'jspdf';
+import html2canvas from 'html2canvas';
 import ListConference from '../Conference/ListConference';
 import * as conferencesStorage from '../../lib/ConferenceStorage';
 
 class Smoothie extends Component {
+
   constructor(props) {
     super(props);
     this.handleClickSave = this.handleClickSave.bind(this);
+    this.handleClickPdf = this.handleClickPdf.bind(this);
   }
 
   handleClickSave() {
     conferencesStorage.setConferencesStore({
       dayOne: this.props.dayOne,
       dayTwo: this.props.dayTwo,
+    });
+  }
+
+  handleClickPdf() {
+    window.html2canvas = html2canvas;
+    const doc = new JsPdf();
+    doc.addHTML(document.getElementsByTagName('body')[0], 15, 15, {
+      width: 170,
+    }, () => {
+      const blob = doc.output('blob');
+      window.open(URL.createObjectURL(blob));
     });
   }
 
@@ -24,6 +39,12 @@ class Smoothie extends Component {
           type="button"
           onClick={this.handleClickSave}
           value="Sauvegarder"
+        />
+        <hr />
+        <input
+          type="button"
+          onClick={this.handleClickPdf}
+          value="Générer pdf"
         />
         <hr />
         <h2>Jour 1</h2>
