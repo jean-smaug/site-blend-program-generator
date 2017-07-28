a7d9e34ed547818945180113339ba0e7860ea226
Chabert Maxime <sin2.chabert.maxime@gmail.com>
2017-07-28 14:42:03 +0200

    add popup when click on save

--- src/components/Smoothie/SmoothieContainer.jsx
+++ src/components/Smoothie/SmoothieContainer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { ToastContainer, ToastMessage } from 'react-toastr';
 import ListConference from '../Conference/ListConference';
 import * as conferencesStorage from '../../lib/ConferenceStorage';
 
@@ -11,6 +12,12 @@ class Smoothie extends Component {
   }
 
   handleClickSave() {
+    this.toast.success(
+      'Votre menu est maintenant enregistré dans votre navigateur.',
+      'Un vrai chef!', {
+        timeOut: 5000,
+        extendedTimeOut: 5000,
+      });
     conferencesStorage.setConferencesStore({
       dayOne: this.props.dayOne,
       dayTwo: this.props.dayTwo,
@@ -20,6 +27,12 @@ class Smoothie extends Component {
   render() {
     return (
       <div>
+        <ToastContainer
+          ref={(input) => { this.toast = input; }}
+          toastMessageFactory={React.createFactory(ToastMessage.animation)}
+          className="toast-top-right"
+        />
+
         <input
           type="button"
           onClick={this.handleClickSave}
