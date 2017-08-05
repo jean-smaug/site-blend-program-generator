19b51afede6e681ce76033bff6b538e66b74fe11
maxime blanc <maximeblanc.dev@gmail.com>
2017-08-05 17:31:12 +0200

    update smoothie unt tests

---
+++ src/app/smoothie/smoothie.spec.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import Smoothie from './smoothie.component';
+
+// const props = {
+//   dayOne: {},
+//   dayTwo: {},
+// };
+
+// describe('Smoothie component', () => {
+//   it('should render Smoothie Component', () => {
+//     const smoothie = shallow(
+//       <Smoothie dayOne={props.dayOne} dayTwo={props.dayTwo} />,
+//     );
+//     expect(smoothie).toMatchSnapshot();
+//   });
+// });
