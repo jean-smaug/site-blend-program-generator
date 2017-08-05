c6916c0413d7389e867474768ff49065fc8334ac
maxime blanc <maximeblanc.dev@gmail.com>
2017-08-05 22:50:36 +0200

    remove useless files

--- src/components/Menu/MenuContainer.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import Menu from './Menu';
-
-export default () => <div><Menu /></div>;
--- src/reducers/reducer.jsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import formReducer from './formReducer';
-
-const allReducers = combineReducers({
-  form: formReducer,
-});
-
-export default allReducers;
