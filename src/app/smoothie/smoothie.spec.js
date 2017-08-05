84cb6c47fe4da28b97ac49fe6a3e5bb78112d68c
maxime blanc <maximeblanc.dev@gmail.com>
2017-08-05 20:58:07 +0200

    add snapshot test for smoothie component

--- src/app/smoothie/smoothie.spec.js
+++ src/app/smoothie/smoothie.spec.js
@@ -1,18 +1,18 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import Smoothie from './smoothie.component';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { SmoothieComponent } from './smoothie.component';
 
-// const props = {
-//   dayOne: {},
-//   dayTwo: {},
-// };
+const props = {
+  dayOne: {},
+  dayTwo: {},
+};
 
 describe('Smoothie component', () => {
   it('should render Smoothie Component', () => {
-    // const smoothie = shallow(
-    //   <Smoothie dayOne={props.dayOne} dayTwo={props.dayTwo} />,
-    // );
-    // expect(smoothie).toMatchSnapshot();
+    const smoothie = shallow(
+      <SmoothieComponent dayOne={props.dayOne} dayTwo={props.dayTwo} />,
+    );
+    expect(smoothie).toMatchSnapshot();
     expect(1).toBe(1);
   });
 });
