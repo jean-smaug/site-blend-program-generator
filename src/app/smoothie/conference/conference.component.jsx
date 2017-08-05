310b10b0dafc9a1478f4f5b8015f478d366e02db
maxime blanc <maximeblanc.dev@gmail.com>
2017-08-05 16:01:37 +0200

    migrate conference component to flow

--- src/app/smoothie/conference/conference.component.jsx
+++ src/app/smoothie/conference/conference.component.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import PropType from 'prop-types';
-import './conference.css';
+// @flow
 
-const Conference = ({ name, timeBegin, timeEnd }) => (
+import React from "react";
+import "./conference.css";
+
+const Conference = ({
+  name,
+  timeBegin,
+  timeEnd
+}: { name: string, timeBegin: number, timeEnd: number }) => (
   <div className="columns">
     <div className="column">
       <div className="conference">
-        <div className="conference-opt"><a><i className="fa fa-arrows-h circle" /></a><span className="conference-time">{`${timeBegin}h00 > ${timeEnd}h00`}</span><a><i className="fa fa-lock circle" /></a></div>
-        <div className="conference-title">{name !== undefined ? name : 'Temps libre'}</div>
+        <div className="conference-opt">
+          <a><i className="fa fa-arrows-h circle" /></a>
+          <span className="conference-time">{`${timeBegin}h00 > ${timeEnd}h00`}</span>
+          <a><i className="fa fa-lock circle" /></a>
+        </div>
+        <div className="conference-title">
+          {name !== undefined ? name : "Temps libre"}
+        </div>
       </div>
     </div>
   </div>
-
 );
-Conference.propTypes = {
-  name: PropType.string.isRequired,
-  timeBegin: PropType.string.isRequired,
-  timeEnd: PropType.string.isRequired,
-};
 
 export default Conference;
