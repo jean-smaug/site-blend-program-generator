17f00decde1b088aa7df6849d96bb74fe6868aa1
maxime blanc <maximeblanc.dev@gmail.com>
2017-08-07 20:32:08 +0200

    init firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+};
+
+firebase.initializeApp(config);
