ba4ef4baa6c2ca268f2672e84b39eb57bd3abbe9
the-smaug <maximeblanc.dev@gmail.com>
2017-08-10 14:16:27 +0200

    Flow (#53)
    * init flow
    
    * migrate conference component to flow
    
    * linter :)
    
    * use flow linter
    
    * add flow as preset
    
    * fix linting problem
    
    * update package.json

--- src/app/smoothie/conference/conference.component.jsx
+++ src/app/smoothie/conference/conference.component.jsx
@@ -1,22 +1,34 @@
+// @flow
+
 import React from 'react';
-import PropType from 'prop-types';
 import './conference.css';
 
-const Conference = ({ name, timeBegin, timeEnd }) => (
-  <div className="columns">
+const Conference = ({
+  name,
+  timeBegin,
+  timeEnd,
+  }: {
+  name: string,
+  timeBegin: number,
+  timeEnd: number
+}) =>
+  (<div className="columns">
     <div className="column">
       <div className="conference">
-        <div className="conference-opt"><a><i className="fa fa-arrows-h circle" /></a><span className="conference-time">{`${timeBegin}h00 > ${timeEnd}h00`}</span><a><i className="fa fa-lock circle" /></a></div>
-        <div className="conference-title">{name !== undefined ? name : 'Temps libre'}</div>
+        <div className="conference-opt">
+          <a>
+            <i className="fa fa-arrows-h circle" />
+          </a>
+          <span className="conference-time">{`${timeBegin}h00 > ${timeEnd}h00`}</span>
+          <a>
+            <i className="fa fa-lock circle" />
+          </a>
+        </div>
+        <div className="conference-title">
+          {name !== undefined ? name : 'Temps libre'}
+        </div>
       </div>
     </div>
-  </div>
-
-);
-Conference.propTypes = {
-  name: PropType.string.isRequired,
-  timeBegin: PropType.string.isRequired,
-  timeEnd: PropType.string.isRequired,
-};
+  </div>);
 
 export default Conference;
