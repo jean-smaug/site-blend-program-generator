09b78de4581e9aa49042a304b0ffa399d85fe30e
Maxime Blanc <maxime.blanc@m6.fr>
2017-08-10 13:12:50 +0200

    use flow linter

--- src/app/smoothie/conference/conference.component.jsx
+++ src/app/smoothie/conference/conference.component.jsx
@@ -1,26 +1,26 @@
 // @flow
 
-import React from "react";
-import "./conference.css";
+import React from 'react';
+import './conference.css';
 
 const Conference = ({
   name,
   timeBegin,
-  timeEnd
-}: { name: string, timeBegin: number, timeEnd: number }) => (
-  <div className="columns">
+  timeEnd,
+  }: { name: string, timeBegin: number, timeEnd: number }) => (
+    <div className="columns">
     <div className="column">
-      <div className="conference">
+        <div className="conference">
         <div className="conference-opt">
-          <a><i className="fa fa-arrows-h circle" /></a>
-          <span className="conference-time">{`${timeBegin}h00 > ${timeEnd}h00`}</span>
-          <a><i className="fa fa-lock circle" /></a>
-        </div>
+            <a><i className="fa fa-arrows-h circle" /></a>
+            <span className="conference-time">{`${timeBegin}h00 > ${timeEnd}h00`}</span>
+            <a><i className="fa fa-lock circle" /></a>
+          </div>
         <div className="conference-title">
-          {name !== undefined ? name : "Temps libre"}
-        </div>
+            {name !== undefined ? name : 'Temps libre'}
+          </div>
       </div>
-    </div>
+      </div>
   </div>
 );
 
