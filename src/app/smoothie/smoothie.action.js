40dfbbf4d1920f0d6b0291de896c29d685a59108
maxime blanc <maximeblanc.dev@gmail.com>
2017-08-14 19:20:24 +0200

    add conference type

--- src/app/smoothie/smoothie.action.js
+++ src/app/smoothie/smoothie.action.js
@@ -1,35 +1,55 @@
-/**
- * Add a conferences to the smoothie state
- * @param conferences
- * @returns {{type: string, data: {word: *}}}
- */
+import {
+  MIX_CONFERENCES,
+  REMOVE_CONFERENCES,
+  SWITCH_CONFERENCE,
+  OPEN_SWITCHER,
+  CLOSE_SWITCHER,
+} from '../constants';
+
+export type Conference = {
+  'day': string,
+  'description': string,
+  'domain': string,
+  'duration': number,
+  'level': string,
+  'room': string,
+  'speaker': {
+    'city': string,
+    'company': string,
+    'gender': string,
+    'linkedinLink': string,
+    'name': string,
+    'pictureLink': string,
+    'twitterLink': string,
+  },
+  'sponsored': boolean,
+  'tags': Array<string>,
+  'timeBegin': string,
+  'title': string,
+};
+
 export const addConferenceAction = conferences => ({
-  type: 'ADD_CONFERENCES',
+  type: MIX_CONFERENCES,
   data: {
     dayOne: conferences.dayOne,
     dayTwo: conferences.dayTwo,
   },
 });
 
-/**
- * remove all conferences to the smoothie state
- * @param conferences
- * @returns {{type: string, data: {word: *}}}
- */
 export const removeConferences = () => ({
-  type: 'REMOVE_CONFERENCES',
+  type: REMOVE_CONFERENCES,
 });
 
 export const switchConference = conference => ({
-  type: 'SWITCH_CONFERENCE',
+  type: SWITCH_CONFERENCE,
   data: { conference },
 });
 
 export const closeSwitcherAction = () => ({
-  type: 'CLOSE_SWITCHER',
+  type: CLOSE_SWITCHER,
 });
 
 export const openSwitcherAction = (currentConferenceId, conferences) => ({
-  type: 'OPEN_SWITCHER',
+  type: OPEN_SWITCHER,
   data: { currentConferenceId, conferences },
 });
