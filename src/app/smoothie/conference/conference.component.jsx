f2e1716a52686a381259febe32fa05293928a65a
Maxime Chabert <sin2.chabert.maxime@gmail.com>
2017-09-27 22:07:01 +0200

    integration fanny, suite demain

--- src/app/smoothie/conference/conference.component.jsx
+++ src/app/smoothie/conference/conference.component.jsx
@@ -72,17 +72,18 @@ export class ConferenceComponent extends Component {
             <div className="conference">
               {conferences.remaining.length > 1 ? (
                 <i
-                  className="fa fa-arrows-h circle"
+                  className="fa fa-arrows-h circle switcher_link"
                   role="presentation"
                   onClick={e => this.openSwitcher(e)}
                 />
               ) : null}
               <div role="button" className="conference-title">
                 {conferences.selected.length !== 0 ? (
-                  _.map(_.orderBy(conferences.selected, 'timeBegin', 'asc'), ({ title, timeBegin, duration }, key) => (
+                  _.map(_.orderBy(conferences.selected, 'timeBegin', 'asc'), ({ title, timeBegin, duration, room }, key) => (
                     <div onClick={() => this.toggleModal(key)} role="presentation" className={`conference conference-${duration}`}>
                       <p className="horaire-preview">{`${timeBegin} > ${getEndTime(timeBegin, duration)}`}</p>
                       <p className="title-preview"> {title.charAt(0).toUpperCase() + title.substring(1).toLowerCase()}</p>
+                      <p className="room-preview"> {`Salle: ${room}`}</p>
                     </div>
                   ))
                 ) : (
