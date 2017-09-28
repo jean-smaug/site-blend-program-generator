9e57c18b499d88514e70dad0dc602e29b6a978ba
Maxime Blanc <maxime.blanc@m6.fr>
2017-09-28 15:06:50 +0200

    destructure in index connection

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -8,56 +8,65 @@ import FormContainer from './blender/blender.component';
 import SmoothieContainer from './smoothie/smoothie.component';
 import { removeConferences } from './smoothie/smoothie.action';
 
-export const App = ({ smoothie, removeConferencesFromState }) =>
-  (<div className="App">
+export const App = ({ smoothie, removeConferencesFromState }) => (
+  <div className="App">
     <div className="header">
       <a href="http://www.blendwebmix.com/">
-        <img src="http://www.blendwebmix.com/wp-content/uploads/2017/04/Blend-Web-Mix-2-1-e1492005334702.png" width="145" height="45" alt="BlendWebMix 2017" />
+        <img
+          src="http://www.blendwebmix.com/wp-content/uploads/2017/04/Blend-Web-Mix-2-1-e1492005334702.png"
+          width="145"
+          height="45"
+          alt="BlendWebMix 2017"
+        />
       </a>
+
+      {!_.isEmpty(smoothie.dayOne) && !_.isEmpty(smoothie.dayTwo) ? (
+        <div className="div-remix column is-2">
+          <input
+            className="btn-remix"
+            type="button"
+            value="Remix"
+            onClick={removeConferencesFromState}
+          />{' '}
+        </div>
+      ) : null}
     </div>
 
     <div className="columns">
       <div className="column  is-12">
-        {_.isEmpty(smoothie.dayOne) && _.isEmpty(smoothie.dayTwo) ?
+        {_.isEmpty(smoothie.dayOne) && _.isEmpty(smoothie.dayTwo) ? (
           <div>
             <div className="column is-6 is-offset-3">
               <img src="https://img11.hostingpics.net/pics/268136header.png" alt="header" />
             </div>
             <FormContainer />
           </div>
-          : null}
+        ) : null}
       </div>
     </div>
     <div className="columns">
       <div className="column  is-12">
-        {!_.isEmpty(smoothie.dayOne) && !_.isEmpty(smoothie.dayTwo)
-          ? <div>
+        {!_.isEmpty(smoothie.dayOne) && !_.isEmpty(smoothie.dayTwo) ? (
+          <div>
             <div className="columns">
-              <div className="div-remix column is-2">
-                <input
-                  className="btn-remix"
-                  type="button"
-                  value="Remix"
-                  onClick={removeConferencesFromState}
-                />{' '}
-              </div>
               <div className="column is-12">
                 <SmoothieContainer />
               </div>
             </div>
           </div>
-          : null}
+        ) : null}
       </div>
     </div>
-  </div>);
+  </div>
+);
 
 App.propTypes = {
   smoothie: SmoothieContainer.propTypes.isRequired,
   removeConferencesFromState: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-  smoothie: state.smoothie,
+const mapStateToProps = ({ smoothie }) => ({
+  smoothie,
 });
 
 const mapDispatchToProps = dispatch => ({
