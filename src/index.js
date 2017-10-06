34d50c001dbb83e140fd4784ad2efe5d7d86431a
maxime blanc <maximeblanc.dev@gmail.com>
2017-10-06 14:58:51 +0200

    remove thunk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -9,7 +8,7 @@ import allReducers from './reducer';
 import App from './app/app.component';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(allReducers, composeWithDevTools(applyMiddleware(logger, thunk)));
+const store = createStore(allReducers, composeWithDevTools(applyMiddleware(logger)));
 
 ReactDOM.render(
   <Provider store={store}>
